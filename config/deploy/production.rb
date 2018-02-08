6874dbb6fcf33ebbc921bb213376c852b1c4f4eb
RKZ <rkz@hyperlogos.net>
2018-02-08 04:29:15 +0100

    capistrano config updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "space.coop", user: "capuser", roles: %w{app web db}
+server "spacedecentral.net", user: "capuser", roles: %w{app web db}
 
 
 # role-based syntax
