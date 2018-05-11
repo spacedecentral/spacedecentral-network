549a69afe756b6400d6aa751edfb682df9c98f1f
Otto G <ottodevs@gmail.com>
2018-05-11 19:43:36 +0200

    Refactor and fix the dynamic social meta implementation. Currently only applied dynamically to posts. If needed for more views is easy to extend by adding the app/view/posts/shows.html.haml "content_for" lines customized to the intended view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["HOST"] || "localhost:3000" }
+    { host: "spacedecentral.net" || "localhost:3000" }
   end
 
   private
