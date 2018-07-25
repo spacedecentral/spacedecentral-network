df22d5eb947c94c8a1ee5bc1916b2671da6a6bad
rkzel <34452131+rkzel@users.noreply.github.com>
2018-07-25 02:04:25 +0100

    Featured (#59)
    * fixed date in Files tab
    
    * new layout and featured projects

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,8 +4,10 @@ class LandingController < ApplicationController
   helper_method :resource_name, :resource, :devise_mapping
 
   def index
+    @featured = Program.where('id = 23')
     @programs = Program.program_type.first(9)
     @projects = Program.project_type.first(9)
-    @trending_posts = Filter::PostFilter.call({ category: Filter::PostFilter::TRENDING }, 1, 6)
+#    @trending_posts = Filter::PostFilter.call({ category: Filter::PostFilter::TRENDING }, 1, 6)
+    @trending_posts = Filter::PostFilter.call({ category: Filter::PostFilter::RECENT }, 1, 6)
   end
 end
