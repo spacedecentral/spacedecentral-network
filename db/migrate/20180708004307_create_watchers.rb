1003ece04c77a8c75bed14aef0056b2beb3aba97
rkzel <rkz@hyperlogos.net>
2018-08-02 12:10:51 +0200

    incorrect watchable_type column type

--- db/migrate/20180708004307_create_watchers.rb
+++ db/migrate/20180708004307_create_watchers.rb
@@ -2,7 +2,7 @@ class CreateWatchers < ActiveRecord::Migration[5.0]
   def change
     create_table :watchers do |t|
       t.references :user, foreign_key: true
-      t.integer :watchable_type
+      t.string :watchable_type
       t.integer :watchable_id
       t.datetime :created_at
 
